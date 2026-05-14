7e7c0b77100b1f01eb93d156f85f2315ec766ef7
alioune badara  faye <aliounebadaraf533@gmail.com>
2026-05-14 18:09:19 +0000

    fix video name

--- app/page.tsx
+++ app/page.tsx
@@ -52,12 +52,7 @@ export default function ForeverProductsSite() {
       {/* Hero */}
       <section className="relative overflow-hidden bg-white">
         <div className="max-w-7xl mx-auto px-6 py-20 grid md:grid-cols-2 gap-12 items-center">
-         <video
-  className="w-full max-w-md mx-auto rounded-2xl"
-  controls
->
-  <source src="/vision.mp4" type="video/mp4" />
-</video>
+          
           <div>
             <h2 className="text-5xl font-extrabold leading-tight mb-6">
               Produits Forever au Sénégal
