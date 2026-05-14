fcc542f3eb2fae025489b77679a685db847d66f9
alioune badara  faye <aliounebadaraf533@gmail.com>
2026-05-14 09:57:04 +0000

    reduire taille images

--- app/page.tsx
+++ app/page.tsx
@@ -107,7 +107,7 @@ export default function ForeverProductsSite() {
               <img
                 src={product.image}
                 alt={product.name}
-                 className="w-40 h-40 object-cover"
+                 className="w-30 h-40 object-cover"
               />
 
               <div className="p-6">
